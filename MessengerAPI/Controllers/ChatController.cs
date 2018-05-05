28d6a297c450c6120f3dd2a91a09c198febc52ac
ShifaaAlaa3 <shifaaalaa11@gmail.com>
2018-05-05 14:07:07 +0200

    User_Messages_Load

--- MessengerAPI/Controllers/ChatController.cs
+++ MessengerAPI/Controllers/ChatController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MessengerAPI.Models.MessengerModel;
 namespace MessengerAPI.Controllers
 {
     public class ChatController : Controller
@@ -12,12 +12,17 @@ namespace MessengerAPI.Controllers
         // GET: Chat
         public ActionResult Chat(int? id)
         {
-            User reciver_user = context.Users.FirstOrDefault(user0 => user0.UserId == id);
+            int senderId = Convert.ToInt32(Session["user_id"]);
+            OldMessages oldMessages = new OldMessages();
+             oldMessages.Receiver= context.Users.FirstOrDefault(user0 => user0.UserId == id);
+             oldMessages.Sender= context.Users.FirstOrDefault(user0 => user0.UserId == senderId);
+              oldMessages.oldMessages = context.UserMessages.Where(m => m.SenderId ==senderId && m.ReceiverId == id && m.SenderId == id && m.ReceiverId == senderId).ToList();
+
             if (id == null)
                 return RedirectToAction("index", "users");
             else
-                if(reciver_user != null)
-                 return View(reciver_user);
+                if(oldMessages.Receiver != null)
+                 return View(oldMessages);
 
             return RedirectToAction("index", "users");
 
---
+++ MessengerAPI/Models/MessengerModel/OldMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessengerAPI.Models.MessengerModel
+{
+    public class OldMessages
+    {
+        public List<UserMessage> oldMessages { get; set; }
+          //  = new List<UserMessage>();
+          public  User Sender { get; set; }
+          public  User Receiver { get; set; }
+
+    }
+}
